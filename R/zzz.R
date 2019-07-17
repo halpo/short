6ec452a14c81f3014238db47fadb71a4979bc7c7
Andrew Redd <Andrew.Redd@hsc.utah.edu>
2019-07-17 12:09:15 -0600

    removed files that were put in the tidymargins package.

--- R/zzz.R
+++ R/zzz.R
@@ -3,4 +3,10 @@
 #' @import methods
 #' @importFrom rlang quos quo enquo expr quo_expr eval_tidy set_names names2 as_function
 #' @importFrom magrittr %$% %<>% %>% %T>%
+#' @importFrom pkgcond %\% %<<% %<<<%
 NULL
+
+assert_that <- pkgcond::assert_that
+message <- pkgcond::pkg_message
+warning <- pkgcond::pkg_warning
+stop <- pkgcond::pkg_error
